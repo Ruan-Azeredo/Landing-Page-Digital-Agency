4bf07461a2b708cc77f92a1f5ffba99e360995aa
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-03-15 20:48:34 -0300

    :construction: build: define colors theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,40 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      'black': {
+        high: '#1F242D',
+        medium: '#272D38',
+        low: '#313846'
+      },
+      'gray': {
+        high: '#B4AFAD',
+        medium: '#DEDCDB',
+        low: '#FFFFFF'
+      },
+      'red': {
+        high: '#BA2E45',
+        medium: '#D34F64',
+        low: '#E28997'
+      },
+      'green': {
+        high: '#238C6E',
+        medium: '#2CAF8A',
+        low: '#42D0A8'
+      },
+      'yellow': {
+        high: '#BD7528',
+        highMedium: '#D89246',
+        medium: '#D8963F',
+        mediumLow: '#E4B581',
+        low: '#F3DEC2'
+      },
+      'blue': {
+        high: '#5A87C2',
+        medium: '#8EADD5',
+        low: '#CFDCED'
+      }
+    },
     extend: {},
   },
   plugins: [],
