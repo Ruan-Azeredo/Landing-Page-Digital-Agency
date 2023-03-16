3392871314085fc88ef5b3a9d4e765ef6864dc64
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-03-16 13:54:20 -0300

    :sparkles: feat: add Navbar a App

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import Navbar from './sections/Navbar';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
         className='w-[450px] h-[450px] bg-blue-medium opacity-25 rounded-full blur-xl
         absolute -top-52 -left-52'
       ></div>
+      <Navbar/>
     </div>
   );
 }
