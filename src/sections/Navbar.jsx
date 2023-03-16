55d83b286e6ca7fe95d2a44cdd6e1e21abfb7423
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-03-16 13:54:56 -0300

    :construction: build: make Navbar

---
+++ src/sections/Navbar.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
+
+const Navbar = () => {
+    const [nav, setNav] = useState(false)
+
+    const handleNav = () => {
+        setNav(!nav)
+    }
+
+    var transition
+    nav ? transition = 'left-0' :  transition = 'left-[-100%]'
+
+    return (
+        <div className='px-6 md:px-40'>
+            <div>
+                
+            </div>
+            <div className='flex py-12 justify-between'>
+
+                <h1 className='font-bold text-base md:uppercase'>Digital Agency</h1>
+
+                <button className='md:hidden' onClick={handleNav}>
+                    {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
+                </button>
+                
+                <ul className='hidden md:flex gap-10 text-lg'>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Testimonials</li>
+                    <li>Contact</li>
+                </ul>
+
+                <ul className={`absolute top-0 bg-gray-low w-[60%] h-full border-r border-gray-high ease-in-out duration-500 ${transition}`}>
+                    <li className='p-4 border-b border-gray-high'>Home</li>
+                    <li className='p-4 border-b border-gray-high'>About</li>
+                    <li className='p-4 border-b border-gray-high'>Testimonials</li>
+                    <li className='p-4 border-b border-gray-high'>Contact</li>
+                </ul>
+
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
