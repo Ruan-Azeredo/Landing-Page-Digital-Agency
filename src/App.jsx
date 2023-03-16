71aa2feae12d2a7301549c006f7256609d353da8
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-03-15 21:03:53 -0300

    :sparkles: feaat: add blue ball

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import './App.css';
 function App() {
   return (
     <div>
-      
+      <div
+        className='w-[450px] h-[450px] bg-blue-medium opacity-25 rounded-full blur-xl
+        absolute -top-52 -left-52'
+      ></div>
     </div>
   );
 }
