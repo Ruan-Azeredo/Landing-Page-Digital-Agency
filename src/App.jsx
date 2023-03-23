61ff4273acdad16aaf6ba4d9e63bc5cec093ecc7
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-03-23 13:54:18 -0300

    :sparkles: build: add infos section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import Companies from './sections/Companies';
 import Hero from './sections/Hero';
+import Infos from './sections/Infos';
 import Navbar from './sections/Navbar';
 
 function App() {
@@ -10,9 +11,14 @@ function App() {
         className='w-[450px] h-[450px] bg-blue-medium opacity-25 rounded-full blur-xl
         absolute -top-52 -left-52 -z-10'
       ></div>
+      <div
+        className='w-[450px] h-[450px] bg-blue-medium opacity-25 rounded-full blur-xl
+        absolute top-[1500px] md:top-[1100px] -right-52 -z-10'
+      ></div>
       <Navbar />
       <Hero />
-      <Companies/>
+      <Companies />
+      <Infos/>
     </div>
   );
 }
